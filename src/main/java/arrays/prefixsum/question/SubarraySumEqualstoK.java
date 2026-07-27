5fb9f71ce059797b3b141dc8091e968ee4d9de80
Saifuddin Shaikh <ssaifuddin473@gmail.com>
2026-07-27 22:07:58 +0530

    repushing

--- src/main/java/arrays/prefixsum/question/SubarraySumEqualstoK.java
+++ src/main/java/arrays/prefixsum/question/SubarraySumEqualstoK.java
@@ -19,4 +19,20 @@ public class SubarraySumEqualstoK {
         }
         return count;
     }
+    public int subarraySumk(int[] arr, int k) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int count=0;
+        int sum=0;
+        map.put(0,1);
+        for(int num: arr){
+            sum+=num;
+            int need=sum-k;
+            if(map.containsKey(need)){
+                count+=map.get(need);
+            }
+            map.put(sum,map.getOrDefault(sum,0)+1);
+
+        }
+        return count;
+    }
 }
