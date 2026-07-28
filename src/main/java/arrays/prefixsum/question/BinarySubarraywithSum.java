061873da90a0cb61f5291c43b6eb46555fcd396a
Saifuddin Shaikh <ssaifuddin473@gmail.com>
2026-07-28 20:39:27 +0530

    solving prefixsum question

--- src/main/java/arrays/prefixsum/question/BinarySubarraywithSum.java
+++ src/main/java/arrays/prefixsum/question/BinarySubarraywithSum.java
@@ -18,4 +18,19 @@ public class BinarySubarraywithSum {
         }
         return count;
     }
+    public int binarysubarraysumk(int[] arr,int k){
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int count =0;
+        int sum=0;
+        map.put(0,1);
+        for(int num:arr){
+            sum+=num;
+            int need=sum-k;
+            if(map.containsKey(need)){
+                count=map.get(need);
+            }
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        }
+        return count;
+    }
 }
