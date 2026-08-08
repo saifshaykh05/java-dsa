b9bfa6435dd2738f9ce41ece441d3e7e43cbb2ca
Saifuddin Shaikh <ssaifuddin473@gmail.com>
2026-08-08 21:44:10 +0530

    done

--- src/main/java/arrays/kandanesAlgorithm/MaximumProduct152.java
+++ src/main/java/arrays/kandanesAlgorithm/MaximumProduct152.java
@@ -25,4 +25,28 @@ public class MaximumProduct152 {
 
         return answer;
     }
+    public int maxProduc(int[] nums) {
+
+        int maximumProduct = nums[0];
+        int minimumProduct = nums[0];
+        int answer = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+
+            int previousMaximum = maximumProduct;
+            int previousMinimum = minimumProduct;
+
+            maximumProduct = Math.max(nums[i],
+                    Math.max(nums[i] * previousMaximum,
+                            nums[i] * previousMinimum));
+
+            minimumProduct = Math.min(nums[i],
+                    Math.min(nums[i] * previousMaximum,
+                            nums[i] * previousMinimum));
+
+            answer = Math.max(answer, maximumProduct);
+        }
+
+        return answer;
+    }
 }
